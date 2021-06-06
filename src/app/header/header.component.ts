9f3c2f4eca66e74a8f8d2cc0830d5550795fb214
Luke Guan <lukeguan@outlook.com>
2021-06-06 15:36:19 -0700

    add http routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
   selector: 'app-header',
@@ -8,6 +9,15 @@ import { Component, EventEmitter, Output } from "@angular/core";
 export class HeaderComponent {
   @Output() featureSelected = new EventEmitter<string>();
 
+  constructor(private dataStorageService: DataStorageService) {}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
   // we don't need it anymore because we have routerlink
   // onSelect(feature: string) {
   //   this.featureSelected.emit(feature) //emit the event when click the button
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,20 +10,26 @@ export class RecipeService {
   //recipeSelected = new Subject<Recipe>();
   recipeChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('A test recipt', 'this is test desc', 'https://cdn2.iconfinder.com/data/icons/bakery-related-line/64/bakery_pastry_cooking-06-512.png',
-    [
-      new Ingredient('Meat', 1),
-      new Ingredient('Shit', 100),
-    ]),
-    new Recipe('B test recipt', 'this is test desc', 'https://cdn2.iconfinder.com/data/icons/bakery-related-line/64/bakery_pastry_cooking-06-512.png',
-    [
-      new Ingredient('Apple', 1),
-      new Ingredient('Pear', 100),
-    ]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('A test recipt', 'this is test desc', 'https://cdn2.iconfinder.com/data/icons/bakery-related-line/64/bakery_pastry_cooking-06-512.png',
+  //   [
+  //     new Ingredient('Meat', 1),
+  //     new Ingredient('Shit', 100),
+  //   ]),
+  //   new Recipe('B test recipt', 'this is test desc', 'https://cdn2.iconfinder.com/data/icons/bakery-related-line/64/bakery_pastry_cooking-06-512.png',
+  //   [
+  //     new Ingredient('Apple', 1),
+  //     new Ingredient('Pear', 100),
+  //   ]),
+  // ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
   }
 
   getRecipes() {
