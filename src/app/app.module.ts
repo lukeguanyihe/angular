e311e942b38ccd9809c77575f70c9e24d379fca7
Luke Guan <lukeguan@outlook.com>
2021-06-29 00:25:54 -0700

    auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,11 +19,11 @@ import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 @NgModule({
   declarations: [
     HeaderComponent,
     AppComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,7 +34,8 @@ import { CoreModule } from './core.module';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    AuthModule
   ],
   bootstrap: [AppComponent],
 })
